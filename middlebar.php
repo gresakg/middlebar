eaaf096df87006e1e516c18e0a6d2a007ffa38be
Gregor Gresak <design@gresak.net>
2016-11-17 11:09:58 +0100

    customizer added

--- middlebar.php
+++ middlebar.php
@@ -13,13 +13,14 @@ new GG_Midlebar();
 
 class GG_Midlebar {
 
-	public $paragraph_offset = 0;
+	public $paragraph_offset = -99;
 
 	public $stop_categories = array('pr_novica');
 
 	public function __construct(){
 		add_action( 'widgets_init', array($this, "widgets_init"),11 );
 		add_filter( 'the_content', array($this, 'inject_sidebar'),10);
+        add_action('customize_register', array($this, 'customizer'));
 	}
 
 	public function inject_sidebar($content) {
@@ -30,17 +31,41 @@ class GG_Midlebar {
 
 		$content = str_replace('</p>', '</p>{{GG}}', $content);
 		$paragraphs = explode('{{GG}}', $content);
-		$n = ceil(count($paragraphs)/2) + $this->paragraph_offset;
+		$n = ceil(count($paragraphs)/2) + $this->get_paragraph_offset();
 		if($n <= 0) {
 			$n = 1;
 		}
-		//var_dump($paragraphs);
+		//var_dump(count($paragraphs),$n);
 		$paragraphs[$n-1] = $paragraphs[$n-1].$middlebar;
 
 		$content = implode("", $paragraphs);
 
 		return $content;
 	}
+
+	protected function get_paragraph_offset() {
+		return get_theme_mod( "middlebar_position", -99 );
+	}
+
+	 public function customizer($wp_customize) {
+        $wp_customize->add_section('middlebar', array(
+            'title' => 'Middlebar',
+            'priority' => 10,
+            ));
+        
+        $wp_customize->add_setting('middlebar_position', array( "default" => -99));
+		$wp_customize->add_control(
+			new WP_Customize_Control(
+				$wp_customize,
+				'middlebar_position',
+				array(
+					'label' => 'Middlebar position',
+					'section' => 'middlebar',
+					'settings' => 'middlebar_position'
+					)
+				)
+			);
+    }
 
 	public function widgets_init()
 	{
